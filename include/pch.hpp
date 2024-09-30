526fb776046d26d6f9f3c7e9b255b61401ae8887
Valentin Muecklausch <valentin.muecklausch@outlook.de>
2024-09-30 16:42:50 +0200

    Initial Commit

---
+++ include/pch.hpp
@@ -0,0 +1,19 @@
+#define SDL_MAIN_HANDLED
+#include "imgui.h"
+#include "imgui_impl_sdl2.h"
+#include "imgui_impl_vulkan.h"
+#include <stdio.h>          // printf, fprintf
+#include <stdlib.h>         // abort
+#include <SDL.h>
+#include <SDL_vulkan.h>
+
+// Volk headers
+#ifdef IMGUI_IMPL_VULKAN_USE_VOLK
+#define VOLK_IMPLEMENTATION
+#include <volk.h>
+#endif
+
+//#define APP_USE_UNLIMITED_FRAME_RATE
+#ifdef _DEBUG
+#define APP_USE_VULKAN_DEBUG_REPORT
+#endif
